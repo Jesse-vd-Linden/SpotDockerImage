724b2bd99913ffb210550cedcc1a9c6a57d8b34b
Jesse-vd-Linden <jesselindenn@gmail.com>
2023-12-30 11:55:39 +0100

    Refactor gesture code

--- catkin_ws/src/gesture_classification/src/gesture_node.py
+++ catkin_ws/src/gesture_classification/src/gesture_node.py
@@ -17,7 +17,7 @@ class GestureClassificationNode:
     
     def __init__(self):
         self.clf = pickle.load(open(Path(__file__).parent.parent.joinpath("models/Poly2SVC.model"), 'rb'))
-        self.ppl = pickle.load(open(Path(__file__).parent.parent.joinpath("models/training.ppl"), 'rb'))
+        self.ppl = pickle.load(open(Path(__file__).parent.parent.joinpath("models/training.ppl"), 'rb')) # sklearn preprocessing pipeline
         self.pub_chatter = rospy.Publisher('/chatter', String, queue_size=2)
         self.frame_buffer = []
         self.sliding_window = 90
@@ -25,9 +25,6 @@ class GestureClassificationNode:
         self.last_execution_time = time.time()
         self.execution_duration = 3 # duration for action execution
 
-        # self.recognition_frequency = 2 # Hz
-        # self.slice_size = int( self.serie_size // self.recognition_frequency )
-        
         self.label_decoding=[
             'fist',#'None'
             'turn_to_left',#'TurnLeft',
@@ -37,52 +34,40 @@ class GestureClassificationNode:
             'walk_to_left',#'GoLeft',
             'walk_to_right',#'GoRight',
         ]
-
-        # self.label_decoding=[
-        #     'stand_up',#'GoLeft',
-        #     'sit_down',#'GoRight',
-        #     'stand_up_high',#'TurnLeft',
-        #     'stop_walking',#'TurnRight',
-        #     'stop_walking', #'Forwards',
-        #     'stop_walking',#'Backwards'
-        # ]
-
+        
+        self.invalid_gesture = "NoGesture"
+        self.vote_threshold = 0.7
+        self.conf_threshold = 0.83
 
     def frequency_query(self, labels: np.ndarray):
         data = Counter(labels)
         return data.most_common(1)[0]
 
-
     def check_threshold(self, labels: np.ndarray, confidence: np.ndarray):
-        # only when most labels are the same and the lowest confidence level is above 0.85, return the gesture
-        invalid_gesture = "NoGesture"
-        vote_threshold = 0.7
-        conf_threshold = 0.83
+        # only when most labels are the same and the lowest confidence level is above 0.83, return the gesture
         label, frequency = self.frequency_query(labels)
-        if frequency/len(labels) < vote_threshold:
-            return invalid_gesture
-        if confidence.mean() < conf_threshold:
-            return invalid_gesture
-        # mannually check the fist gesture
-        if label == 0:
-            return invalid_gesture
-        # print(f"current convidance: {confidence.mean()}; current label {self.label_decoding[label]}")
+        if frequency/len(labels) < self.vote_threshold:
+            return self.invalid_gesture
+        if confidence.mean() < self.conf_threshold:
+            return self.invalid_gesture
+        if label == 0: # manually check the fist gesture
+            return self.invalid_gesture
+        
         return self.label_decoding[label]
-
 
     def recognize_gesture(self):
         current_frame_buffer = np.array(self.frame_buffer).reshape(-1, 78)
         x = self.ppl.transform(current_frame_buffer)
         y = self.clf.predict_proba(x)
+        
         labels = np.argmax(y, axis=1)
         confidence = np.max(y, axis=1)
         res = self.check_threshold(labels, confidence)
-        # print(f"current gesture: {res}")
         current_time = time.time()
         if res != self.last_gesture:
             if res == "NoGesture":
                 # publish and continue
-                # self.pub_chatter.publish(f"{res}")
+                self.pub_chatter.publish(f"{res}")
                 self.last_gesture = res
             else:
                 # wait for the execution of the last valid action
@@ -97,15 +82,13 @@ class GestureClassificationNode:
         else:
             self.frame_buffer.pop(0)
             self.frame_buffer.append(data.data)
-            # while True and not rospy.is_shutdown():
+
             self.recognize_gesture()
-                # rospy.sleep(0.5)
     
     def callback_show_keypoints(self, data):
         array = np.array(data.data).reshape(-1,78)
         self.hand_keypoint_list.append(array)
         print(array)
-
 
     def run(self):
         rospy.init_node('gesture_recognizer', anonymous=True)
